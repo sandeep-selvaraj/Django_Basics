af424f4a484281dcee3f58b78b27b0a51d39510e
sandeep-selvaraj <selvaraj.sandeep@yahoo.co.in>
2022-03-04 18:10:47 +0530

    second_changes

--- second_project/second_project/urls.py
+++ second_project/second_project/urls.py
@@ -1,4 +1,4 @@
-"""first_project URL Configuration
+"""second_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -15,10 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from first_app import views
-
 
 urlpatterns = [
-    path('',views.index, name='index' ),
     path('admin/', admin.site.urls),
 ]
